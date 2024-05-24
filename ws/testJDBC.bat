53d4747009a5153bbe19f6a442e271460ce6f4ba
Tay <brayan.campion1@gmail.com>
2024-05-24 10:54:18 +0200

    testJDBC modified:   ws/batman.bat modified:   ws/testJDBC.bat

--- ws/testJDBC.bat
+++ ws/testJDBC.bat
@@ -1,1 +1,1 @@
-javac --module-path "C:\Users\omerf\Desktop\SAE\SAE 2.0 - Communes\App\javafx-sdk-22.0.1\lib" --add-modules javafx.controls -d ../class ../src/view/*.java ../src/controller/*.java ../src/model/*.java ../src/*.java
+javac --module-path "C:\COURS\P4\SAE\SAE_Communes_Bretonne\lib\javafx-sdk-22.0.1\lib" --add-modules javafx.controls -d ../class ../src/view/*.java ../src/controller/*.java ../src/model/*.java ../src/*.java
