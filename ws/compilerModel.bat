3eee6f4a1ef93e54a78e9fb5ada4cf0f3b87786f
RomainPERON <romainperon29750@gmail.com>
2024-05-27 12:09:35 +0200

    modif modelcompiler modified:   ws/compilerModel.bat

--- ws/compilerModel.bat
+++ ws/compilerModel.bat
@@ -1,5 +1,7 @@
-javac -d ../class ../src/model/exceptions/*.java
-javac -d ../class ../src/model/*.java
+javac -d ../class ../src/model/data/exceptions/*.java
+javac -d ../class ../src/model/data/*.java
+javac -d ../class ../src/model/dao/*.java
+javac -d ../class ../src/*.java
 
 
 java ScenarioSAE
